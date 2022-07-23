20709e29712a7ab21005f15885e4613d7e0d3fb8
Mike <mike@roetto.org>
2022-07-23 09:31:35 -0400

    copyright and license notices

--- holdings.php
+++ holdings.php
@@ -1,3 +1,5 @@
+<!--Copyright (C) 2022 Mike Roetto <mike@roetto.org>
+SPDX-License-Identifier: GPL-3.0-or-later-->
 <html>
 
 <head>
--- nav.php
+++ nav.php
@@ -1,3 +1,6 @@
+    <!--Copyright (C) 2022 Mike Roetto <mike@roetto.org>
+    SPDX-License-Identifier: GPL-3.0-or-later-->
+
 <?php
 
 $dir = 'sqlite:portfolio.sqlite';
