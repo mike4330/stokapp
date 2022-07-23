5383e91ce1012d5d666f46c807dca5acfc1f3289
Mike <mike@roetto.org>
2022-07-23 08:29:07 -0400

    1st

---
+++ data.php
@@ -0,0 +1,109 @@
+
+
+<?php
+header('Content-Type: application/json');
+
+$dir = 'sqlite:portfolio.sqlite';
+
+$dbh  = new PDO($dir) or die("cannot open the database");
+
+// echo "q $_GET[q]\n";
+
+
+if ($_GET[q] == "divs") { 
+//     echo "ok\n";
+//     echo "ok2\n";
+    $query = "select sum(price*units) as cost,substr(date_new,1,7) as p from transactions where xtype = 'Div' group by p ";
+    }
+
+if ($_GET[q] == "valuetrend") {
+    $query= "select date,value,cost from historical where date > date('now','-180 days')";
+    }
+    
+if ($_GET[q] == "averages") {
+    $query= "select date,WMA8,WMA24,WMA28,WMA36,WMA48,WMA41,WMA55,WMA64,return as rtn from historical where date > date('now','-60 days')";
+    }
+    
+if ($_GET[q] == "quarterdivs") {
+    $query= "select sum(units*price) as total,(strftime('%Y', date_new)) || 'Q' || ((strftime('%m', date_new) + 2) / 3) as q 
+    from transactions where xtype = 'Div' group by q";
+    }
+    
+
+if ($_GET[q] == "portpct") {
+
+$v=get_portfolio_value();
+// echo $v;
+$query = "SELECT DISTINCT symbol FROM transactions order by symbol";
+foreach ($dbh->query($query) as $row) {
+        $sym=$row[symbol];
+        $subquery = "select sum(units) as buyunits from transactions where xtype = 'Buy' and symbol = '$sym'";
+        $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $buyunits = $zrow['buyunits'];
+        $subquery = "select sum(units) as sellunits from transactions where xtype = 'Sell' and symbol = '$sym'";
+        $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $sellunits = $zrow[sellunits];
+        $netunits = ($buyunits-$sellunits);
+        
+        if ($netunits == 0) continue;
+        
+        $subquery = "select price from prices where symbol = '$sym'";
+        $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $curprice = $zrow[price];
+        
+        $pos_value=($curprice * $netunits);
+        
+        $pos_pct=($pos_value / $v)*100;
+        
+//         echo "$sym $curprice $pos_value $pos_pct\n";
+        if ($pos_pct > 2) {$array[] = array('symbol'=> "$sym", 'pos_pct'=> "$pos_pct");}
+            else {$otherpct = $otherpct + $pos_pct;}
+//         $array[$sym] = $pos_pct;
+       
+         
+    }
+    
+
+$array[] = array('symbol'=> "Other", 'pos_pct'=> "$otherpct");
+    
+// asort($array,SORT_NUMERIC );
+// print_r($array);
+
+
+echo json_encode($array); 
+exit;
+}
+
+
+
+$sth = $dbh->prepare($query);
+$sth->execute();
+
+$data = $sth->fetchall(PDO::FETCH_ASSOC);
+
+echo json_encode($data);
+
+
+
+function get_portfolio_value() {
+//     echo "executing function<br>";
+    $dir = 'sqlite:portfolio.sqlite';
+    $dbh  = new PDO($dir) or die("cannot open the database");
+    $q = "SELECT DISTINCT symbol FROM transactions order by symbol";
+    foreach ($dbh->query($q) as $trow) {
+        $tsym=$trow[symbol];
+        $subquery = "select sum(units) as buyunits from transactions where xtype = 'Buy' and symbol = '$tsym'";
+        $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $buyunits = $zrow['buyunits'];
+        $subquery = "select sum(units) as sellunits from transactions where xtype = 'Sell' and symbol = '$tsym'";
+        $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $sellunits = $zrow[sellunits];
+        $netunits = ($buyunits-$sellunits);
+        $subquery = "select price from prices where symbol = '$tsym'";
+        $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $cprice = $zrow[price];
+        
+        if ($netunits == 0) continue;
+        
+        $value = round(($netunits * $cprice),3);
+        $ttotal = ($ttotal + $value);
+//      echo "total $ttotal <br>";
+    }
+return $ttotal;
+}
+?>
+
---
+++ datacsv.php
@@ -0,0 +1,241 @@
+
+
+<?php
+
+header('Content-Type: application/json');
+
+$dir = 'sqlite:portfolio.sqlite';
+
+$dbh  = new PDO($dir) or die("cannot open the database");
+
+if ($_GET[q] == "cumshares") {
+    $isym = $_GET[symbol]; 
+    if ($isym == "BRKB") {$isym ="BRK.B";}
+    
+    $query= "select date_new,symbol,units,xtype 
+    from transactions 
+    where symbol = '$isym' AND (xtype = 'Buy' OR xtype = 'Sell')";
+    
+    
+    #echo "date,symbol,shares\n";
+    foreach ($dbh->query($query) as $row) {
+        $xtype=$row[xtype];
+        if ($xtype == 'Sell') {$cumshares = ($cumshares - $row[units]);}
+            else {$cumshares = ($cumshares + $row[units]);}
+        $array[] = array('cumshares'=> "$cumshares", 'date'=> $row[date_new]);
+        }
+        $cd=date("Y-m-d");
+//         $tomorrow  = mktime(0, 0, 0, date("Y")  , date("m"), date("d")+1);
+        $array[] = array('cumshares'=> "$cumshares", 'date'=> $cd);
+    }
+    
+
+if ($_GET[q] == "cumshares2") {
+    $isym = $_GET[symbol]; 
+    if ($isym == "BRKB") {$isym ="BRK.B";}
+    
+    $query= "select timestamp,shares from security_values where symbol = '$isym' 
+    AND  timestamp > date('now','-18 months') order by timestamp";
+    
+    #echo "date,symbol,shares\n";
+    foreach ($dbh->query($query) as $row) {
+        $array[] = array('cumshares'=> "$row[shares]", 'date'=> "$row[timestamp]");
+    }
+ }   
+ 
+    
+if ($_GET[q] == "cumvalue") {
+    $isym = $_GET[symbol]; 
+    
+    $query= "select date_new,symbol,units,xtype 
+    from transactions 
+    where symbol = '$isym' AND (xtype = 'Buy' OR xtype = 'Sell')";
+    #echo "date,symbol,shares\n";
+    foreach ($dbh->query($query) as $row) {
+        $xtype=$row[xtype];
+        if ($xtype == 'Sell') {$cumshares = ($cumshares - $row[units]);}
+            else {$cumshares = ($cumshares + $row[units]);}
+        $array[] = array('cumshares'=> "$cumshares", 'date'=> $row[date_new]);
+        }
+        $cd=date("Y-m-d");
+//         $tomorrow  = mktime(0, 0, 0, date("Y")  , date("m"), date("d")+1);
+        $array[] = array('cumshares'=> "$cumshares", 'date'=> $cd);
+    }    
+
+    
+if ($_GET[q] == "posvalues") {
+//     echo $_GET[symbol];
+    
+    $isym = $_GET[symbol];
+    if ($isym == "BRKB") {$isym ="BRK.B";}
+    $query = "select timestamp,(close*shares) as pval from security_values where symbol = '$isym' AND  timestamp > date('now','-365  days') order by timestamp";
+//     echo $query;
+    foreach ($dbh->query($query) as $row) {
+//         echo "$row[pval]\n";
+        $array[] = array('posvalue'=> "$row[pval]", 'date'=> $row[timestamp]);
+    }
+}
+
+if ($_GET[q] == "secprices") {
+//     echo $_GET[symbol];
+    
+    $isym = $_GET[symbol];
+    if ($isym == "BRKB") {$isym ="BRK.B";}
+    $query = "select timestamp,close from security_values where symbol = '$isym' AND  timestamp > date('now','-365  days') order by timestamp";
+//     echo $query;
+    foreach ($dbh->query($query) as $row) {
+//         echo "$row[pval]\n";
+        $array[] = array('posvalue'=> "$row[close]", 'date'=> $row[timestamp]);
+    }
+}
+
+if ($_GET[q] == "gl") {
+
+$query = "SELECT DISTINCT symbol FROM transactions order by symbol";
+foreach ($dbh->query($query) as $row) {
+    $sym=$row[symbol];
+    
+    $subquery = "select sum(units) as buyunits from transactions where xtype = 'Buy' and symbol = '$sym'";
+    $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $buyunits = $zrow['buyunits'];
+    
+    $subquery = "select sum(units) as sellunits from transactions where xtype = 'Sell' and symbol = '$sym'";
+    $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $sellunits = $zrow[sellunits];
+    
+    $subquery = "select price from prices where symbol = '$sym'";
+    $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $cprice = $zrow[price];
+    
+    $netunits = ($buyunits-$sellunits);
+    if ($netunits == 0) continue;
+    
+    $subquery="SELECT sum(units*price) AS buytotal FROM transactions WHERE xtype = 'Buy' AND symbol = '$sym'";
+    $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $buytotal = round($zrow[buytotal],3);
+        
+    $subquery="SELECT sum(units*price) AS selltotal FROM transactions WHERE xtype = 'Sell' AND symbol = '$sym'";
+    $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $selltotal = round($zrow[selltotal],3);
+    
+    $subquery="SELECT sum(gain) as rgain FROM transactions WHERE xtype = 'Sell' AND symbol = '$sym'";
+    $stmt = $dbh->prepare($subquery);$stmt->execute();$zrow = $stmt->fetch(); $gain = round($zrow[rgain],3);
+    
+    $posvalue = round(($netunits * $cprice),3);
+    $netcost = round(($buytotal - $selltotal),3)+$gain;
+    $dollarreturn=round(($posvalue - $netcost),3);
+    
+    $array[] = array('symbol'=> "$row[symbol]",'dollarreturn'=>"$dollarreturn" );
+    asort($array, SORT_NUMERIC);
+    }
+}
+
+if (!empty($_GET['symquery'])) {
+    $symbol = $_GET['symquery'];
+//     echo "symquery for $symbol\n";
+    $query = "select substr(date_new,0,8) as month,symbol,sum(price*units) as cost 
+    from transactions where symbol = '$symbol' 
+    and xtype = 'Div' group by month";    
+    foreach ($dbh->query($query) as $row) {
+       
+     $array[] = array('date'=> "$row[month]",'symbol'=> "$row[symbol]",'cost'=>"$row[cost]" );   
+    }
+}
+
+
+if ( $_SERVER['QUERY_STRING'] == "catpct") {
+    $tf=290;
+    $query = "select timestamp,(sum(shares*close)/value)*100 as pct
+    from security_values,historical 
+    where symbol IN 
+    (select symbol from prices where asset_class = 'Commodities')
+    and security_values.timestamp = historical.date
+    and timestamp > date('now','-$tf days')
+    group by timestamp order by timestamp ";
+
+    foreach ($dbh->query($query) as $row) {
+        $ix++;
+        $array[] = array('date'=> "$row[timestamp]",'pctcomms'=> "$row[pct]");
+        
+    }
+    
+    $ix=0;
+    
+    $query = "select timestamp,(sum(shares*close)/value)*100 as pct
+    from security_values,historical 
+    where symbol IN 
+    (select symbol from prices where asset_class LIKE '%Stock')
+    and security_values.timestamp = historical.date
+    and timestamp > date('now','-$tf days')
+    group by timestamp order by timestamp";
+    
+    foreach ($dbh->query($query) as $row) {
+        $array[$ix]["pctstock"]="$row[pct]";
+        $ix++;
+    }
+    
+    $ix=0;
+    
+     $query = "select timestamp,(sum(shares*close)/value)*100 as pct
+    from security_values,historical 
+    where symbol IN 
+    (select symbol from prices where asset_class LIKE '%Bonds')
+    and security_values.timestamp = historical.date
+    and timestamp > date('now','-$tf days')
+    group by timestamp order by timestamp";
+     
+         foreach ($dbh->query($query) as $row) {
+        $array[$ix]["pctbonds"]="$row[pct]";
+        $ix++;
+    }
+
+    
+        $ix=0;
+    
+     $query = "select timestamp,(sum(shares*close)/value)*100 as pct
+    from security_values,historical 
+    where symbol IN 
+    (select symbol from prices where symbol IN ('SGOL','XAG')
+    and security_values.timestamp = historical.date)
+    and timestamp > date('now','-$tf days')
+    group by timestamp order by timestamp";
+     
+         foreach ($dbh->query($query) as $row) {
+        $array[$ix]["pctpm"]="$row[pct]";
+        $ix++;
+    }
+
+    
+    
+}
+ 
+
+if (!empty($_GET['symreturn'])) {
+    $symbol = $_GET['symreturn'];
+    
+    if (!empty($_GET['tf'])) {$days = $_GET['tf']; } 
+        else {$days = 365;}
+        
+        
+//     echo "symquery for $symbol\n";
+    $query = "SELECT timestamp,symbol,cost_basis,
+    close*shares as value,close,cbps,
+    (((shares*close)-(cost_basis)+cum_divs)/cost_basis)*100 as rtn
+    FROM security_values WHERE symbol = '$symbol' AND timestamp > date('now','-$days  days') ORDER BY timestamp ";    
+    
+    foreach ($dbh->query($query) as $row) {
+        $array[] = array('date'=> "$row[timestamp]",'symbol'=> "$row[symbol]",
+        'cost'=>"$row[cost_basis]",'value'=>"$row[value]",
+        'close'=>"$row[close]",'cbps'=>"$row[cbps]",'rtn'=>"$row[rtn]" );   
+    }
+}
+
+if ($_GET['q'] == "test") {
+$query = "SELECT symbol,timestamp,(((shares*close)-(cost_basis)+cum_divs)/cost_basis)*100 as rtn
+FROM security_values 
+WHERE timestamp > date('now','-395  days') ORDER BY timestamp "; 
+ 
+ foreach ($dbh->query($query) as $row) {
+        $array[] = array('date'=> "$row[timestamp]", 'symbol' => $row['symbol'], 'rtn' => $row['rtn']);   
+    }
+ 
+}
+
+echo json_encode($array);   
+//  print_r($array);
+?>
