913376de7097c23eb291260bed18667bf9a2bde2
Mike Roetto <mike@roetto.org>
2024-04-05 12:13:44 -0400

    updates

--- div.php
+++ div.php
@@ -2,246 +2,248 @@
 SPDX-License-Identifier: GPL-3.0-or-later-->
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="main.css">
-<link rel="stylesheet" type="text/css" href="nav.css">
-<title>Dividends</title>
 
-<!-- <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script> -->
-<script src="/js/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="/js/chart.js"></script> 
+<head>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <link rel="stylesheet" type="text/css" href="nav.css">
+  <title>Dividends</title>
+
+  <!-- <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script> -->
+  <script src="/js/jquery-3.1.1.min.js"></script>
+  <script type="text/javascript" src="/js/chart.js"></script>
 
 </head>
+
 <body>
 
-<?php
-$dir = 'sqlite:portfolio.sqlite';
-include ("nav.php"); 
-?>
-    <table class="chart2">
+  <?php
+  $dir = 'sqlite:portfolio.sqlite';
+  include ("nav.php");
+  ?>
+  <table class="chart2">
     <tr class="chart">
-    <td><canvas id="mychart" style="width:46vw;height:44vh;">"</canvas></td>
-    <td><canvas id="valuechart" style="width:46vw;height:44vh;"></canvas></td>
+      <td><canvas id="mychart" style="width:46vw;height:44vh;">"</canvas></td>
+      <td><canvas id="valuechart" style="width:46vw;height:44vh;"></canvas></td>
     </tr>
-    
-    <tr>
-    <td><canvas id="quarterdivs" style="width:46vw;height:44vh;"></canvas></td>
-    <td><canvas id="averageschart" style="width: 46vw;height:44vh;"></canvas></td>
-    </tr></table>
 
-    <script>
-        
-$.ajax({
+    <tr>
+      <td><canvas id="quarterdivs" style="width:46vw;height:44vh;"></canvas></td>
+      <td><canvas id="averageschart" style="width: 46vw;height:44vh;"></canvas></td>
+    </tr>
+  </table>
+
+  <script>
+
+    $.ajax({
       url: 'data.php?q=divs',
       type: 'GET',
       dataType: 'json',
-      success:function(data){
+      success: function (data) {
 
         var cost = [];
         var p = [];
 
-        for(var i in data){
+        for (var i in data) {
           cost.push(data[i].cost);
           p.push(data[i].p);
         }
 
         var chartdata = {
-            labels: p,
-            datasets: [
-              {
-                label: 'Div Amount',
-                backgroundColor: 'rgb(90, 90, 132)',
-                data:cost
-              }
-            ] 
+          labels: p,
+          datasets: [
+            {
+              label: 'Div Amount',
+              backgroundColor: 'rgb(88, 88, 200)',
+              data: cost
+            }
+          ]
         };
 
         var ctx = $('#mychart');
         var barGraph = new Chart(ctx, {
-            type:'bar',
-            data: chartdata,  
-            options: {responsive: false}
+          type: 'bar',
+          data: chartdata,
+          options: { responsive: false }
         });
       },
     });
-    
-  $.ajax({
-    url: 'data.php?q=valuetrend',
-    type: 'GET',
-    dataType: 'json',
-    success:function(data){
-    
-        var value = [];var date = [];var cost = [];
-         
-         for(var i in data){
+
+    $.ajax({
+      url: 'data.php?q=valuetrend',
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+
+        var value = []; var date = []; var cost = [];
+
+        for (var i in data) {
           date.push(data[i].date);
           value.push(data[i].value);
           cost.push(data[i].cost);
         }
-    
+
         var chartdata = {
-            labels: date,
-            datasets: [
-              {
-                label: 'Value',
-                radius: 0,
-                borderColor: 'rgb(255, 0, 0)',
-                borderWidth: 1,
-                tension: .9,
-                data:value
-              },
+          labels: date,
+          datasets: [
             {
-                label: 'Cost',
-                borderColor: 'rgb(0, 0, 255)',
-                borderWidth: 1,
-                radius: 0,
-                data:cost
-              }
-            ] 
-            };
-            
+              label: 'Value',
+              radius: 0,
+              borderColor: 'rgb(255, 0, 0)',
+              borderWidth: 1,
+              tension: .9,
+              data: value
+            },
+            {
+              label: 'Cost',
+              borderColor: 'rgb(0, 0, 255)',
+              borderWidth: 1,
+              radius: 0,
+              data: cost
+            }
+          ]
+        };
+
         var ctx = $('#valuechart');
         var barGraph = new Chart(ctx, {
-            type:'line',
-            data: chartdata, 
-            options: {
-                responsive: false,
-            }
+          type: 'line',
+          data: chartdata,
+          options: {
+            responsive: false,
+          }
         });
-    }
-  });  
+      }
+    });
 
-   $.ajax({
-  
-    url: 'data.php?q=averages',
-    type: 'GET',
-    dataType: 'json',
-    success:function(data){
-    
-        var WMA8 = [];var date = [];
-        var WMA24 = []; var WMA28 = []; var WMA36=[]; var WMA41=[]; 
-        var WMA48=[];var rtn=[]; var WMA64=[]; var WMA72=[]; 
-        var WMA88=[]; var WMA110=[]; var WMA135=[];
-         
-         for(var i in data){
+    $.ajax({
+
+      url: 'data.php?q=averages',
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+
+        var WMA8 = []; var date = [];
+        var WMA24 = []; var WMA28 = []; var WMA36 = []; var WMA41 = [];
+        var WMA48 = []; var rtn = []; var WMA64 = []; var WMA72 = [];
+        var WMA88 = []; var WMA110 = []; var WMA135 = [];
+
+        for (var i in data) {
           date.push(data[i].date);
-          WMA8.push(data[i].WMA8);WMA24.push(data[i].WMA24);
-          WMA28.push(data[i].WMA28);WMA36.push(data[i].WMA36);
-          WMA41.push(data[i].WMA41);WMA48.push(data[i].WMA48);
+          WMA8.push(data[i].WMA8); WMA24.push(data[i].WMA24);
+          WMA28.push(data[i].WMA28); WMA36.push(data[i].WMA36);
+          WMA41.push(data[i].WMA41); WMA48.push(data[i].WMA48);
           WMA64.push(data[i].WMA64);
           WMA72.push(data[i].WMA72); WMA88.push(data[i].WMA88); WMA110.push(data[i].WMA110);
           WMA135.push(data[i].WMA135);
           rtn.push(data[i].rtn);
         }
 
-        var chartdata = { 
+        var chartdata = {
           labels: date,
-            datasets: [
-              {
-                label: '8WMA',
-                radius: 0,
-                borderColor: 'rgb(255, 0, 0)',
-                borderWidth: 2.5,
-                data: WMA8
-              },
-              {
-                label: '72WMA',
-                radius: 0,
-                borderColor: 'rgb(32, 54, 240)',
-                borderWidth: 2.5,
-                data: WMA72
-              },
+          datasets: [
+            {
+              label: 'return',
+              radius: 1.5,
+              borderColor: 'rgb(32, 32, 32)',
+              borderWidth: 1,
+              data: rtn
+            },
+            {
+              label: '48WMA',
+              radius: 0,
+              borderColor: 'rgb(255, 0, 0)',
+              borderWidth: 2.5,
+              data: WMA48
+            },
+            {
+              label: '88WMA',
+              radius: 0,
+              borderColor: 'rgb(32, 54, 240)',
+              borderWidth: 2.5,
+              data: WMA88
+            },
+            //       {
+            //   label: 'WMA36',
+            //   radius: 0,
+            //   borderColor: 'rgb(255,255,0)',
+            //   borderWidth: 2.5,
+            //   data: WMA36
+            // },
+            {
+              label: 'WMA110',
+              radius: 0,
+              borderColor: 'rgb(30,225,30)',
+              borderWidth: 2.5,
+              data: WMA110
+            },
 
-               {
-                label: 'return',
-                radius: 1.5,
-                borderColor: 'rgb(32, 32, 32)',
-                borderWidth: 1,
-                data: rtn
-              },
- 
-                    {
-                label: 'WMA36',
-                radius: 0,
-                borderColor: 'rgb(255,255,0)',
-                borderWidth: 2.5,
-                data: WMA36
-              },
-                     {
-                label: 'WMA110',
-                radius: 0,
-                borderColor: 'rgb(30,225,30)',
-                borderWidth: 2.5,
-                data: WMA110
-              },
-
-                     {
-                label: 'WMA135',
-                radius: 0,
-                borderColor: 'rgb(125,125,255)',
-                borderWidth: 2.5,
-                data: WMA135
-              }
-            ] 
-            };
-            
-            
-        var ctx = $('#averageschart');        
-        var barGraph = new Chart(ctx, {
-            type:'line',
-            data: chartdata,  
-            options: {
-                responsive: false,
-                
+            {
+              label: 'WMA135',
+              radius: 0,
+              borderColor: 'rgb(198,178,0)',
+              borderWidth: 2.5,
+              data: WMA135
             }
+          ]
+        };
+
+
+        var ctx = $('#averageschart');
+        var barGraph = new Chart(ctx, {
+          type: 'line',
+          data: chartdata,
+          options: {
+            responsive: false,
+
+          }
         });
-    }
-  
-  }); 
- 
- 
-   $.ajax({
-  
-    url: 'data.php?q=quarterdivs',
-    type: 'GET',
-    dataType: 'json',
-    success:function(data){
-    
-        var q = [];var total = [];
-        
-         
-         for(var i in data){
+      }
+
+    });
+
+
+    $.ajax({
+
+      url: 'data.php?q=quarterdivs',
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+
+        var q = []; var total = [];
+
+
+        for (var i in data) {
           q.push(data[i].q);
           total.push(data[i].total);
         }
-    
+
         var chartdata = {
-            labels: q,
-            datasets: [
-              {
-                label: 'total',
-                radius: 0,
-                backgroundColor: 'rgb(16, 192, 16)',
-                data: total
-              }
-            ] 
-            };
-            
-            
-        var ctx = $('#quarterdivs');
-        
-        
-        var barGraph = new Chart(ctx, {
-            type:'bar',
-            data: chartdata,  
-            options: {
-                responsive: false,
+          labels: q,
+          datasets: [
+            {
+              label: 'total',
+              radius: 0,
+              backgroundColor: 'rgb(16, 172, 16)',
+              data: total
             }
+          ]
+        };
+
+
+        var ctx = $('#quarterdivs');
+
+
+        var barGraph = new Chart(ctx, {
+          type: 'bar',
+          data: chartdata,
+          options: {
+            responsive: false,
+          }
         });
-    }
-  
-  }); 
- 
- </script>
+      }
+
+    });
+
+  </script>
 </body>
+
 </html>
